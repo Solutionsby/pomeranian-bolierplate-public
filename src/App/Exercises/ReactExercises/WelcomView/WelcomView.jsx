3eaecd53d41c8adf1bc3424eb5ff1c0c0096d20c
Paweł Goc <pawel.goc@solutionsby.pl>
2023-04-27 19:07:20 +0200

    Add New site

--- src/App/Exercises/ReactExercises/WelcomView/WelcomView.jsx
+++ src/App/Exercises/ReactExercises/WelcomView/WelcomView.jsx
@@ -1,5 +1,17 @@
 import './styles.css';
 
 export const WelcomeView = () => {
-  return <div>Welcome</div>;
+  return (
+    <div className="wrapper">
+      <h1>Cześć</h1>
+      <form>
+        <label>
+          <p>Wpisz Tekst</p>
+          <input type="text" placeholder="Wpisz Tekst" />
+        </label>
+      </form>
+      <button className="someBtn">Kliknij</button>
+      <div className="placeHolder">PlaceHolder</div>
+    </div>
+  );
 };
--- src/App/Exercises/ReactExercises/WelcomView/router-data.jsx
+++ src/App/Exercises/ReactExercises/WelcomView/router-data.jsx
@@ -3,7 +3,7 @@ import { WelcomeView } from './WelcomView';
 export const welcomeVievRouterMetaData = {
   path: 'WelcomeView',
   date: '27-04-2023',
-  linkLabel: 'WelcomeView',
+  linkLabel: 'Welcome view',
   blockNo: 1,
   element: <WelcomeView />,
   tags: ['components'],
