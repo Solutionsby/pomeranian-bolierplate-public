f886bbe33b0a7cdaed3b7abf7d108f4b7ac95dd8
Paweł Goc <pawel.goc@solutionsby.pl>
2023-04-28 18:03:22 +0200

    Feature with count Click

---
+++ src/App/Exercises/ReactExercises/ButtonCount/ButtonCount.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import './styles.css';
+
+export const ButtonCount = () => {
+  let [count, setCount] = useState(1);
+  let x = 'wanish';
+  if (count >= 5) {
+    x = 'activeText';
+    count = 0;
+  }
+
+  return (
+    <div className="wrapper">
+      <h1>Kliknij 5 razy</h1>
+      <button className="someBtn" onClick={() => setCount(count + 1)}>
+        Kliknełeś {count} razy
+      </button>
+      <div className={x}>Ukryta wiadomość </div>
+    </div>
+  );
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -1,4 +1,5 @@
 import { getParsedRouterDataList } from '../../router-data/parseRouterData';
+import { ButtonCountVievRouterMetaData } from './ButtonCount/router-data';
 
 import { SubRouteExampleMetaData } from './SubRouteExample/router-data';
 import { welcomeVievRouterMetaData } from './WelcomView/router-data';
@@ -6,5 +7,6 @@ import { welcomeVievRouterMetaData } from './WelcomView/router-data';
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
   welcomeVievRouterMetaData,
+  ButtonCountVievRouterMetaData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
