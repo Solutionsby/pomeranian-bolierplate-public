6b316c3789807a6605cff69bfce2e1949e285ff1
Paweł Goc <pawel.goc@solutionsby.pl>
2023-04-28 19:01:09 +0200

    if and else if

--- src/App/Exercises/ReactExercises/ButtonCount/ButtonCount.jsx
+++ src/App/Exercises/ReactExercises/ButtonCount/ButtonCount.jsx
@@ -2,21 +2,24 @@ import { useState } from 'react';
 import './styles.css';
 
 export const ButtonCount = () => {
-  let [count, setCount] = useState(1);
+  let [count, setCount] = useState(0);
   const [show, SetShow] = useState(false);
 
   const HandleClick = () => {
-    if (count < 5) {
+    if (count < 4) {
       setCount(count + 1);
       SetShow(false);
+    } else if (count < 5) {
+      SetShow(true);
+      setCount(count + 1);
     } else {
       setCount(0);
-      SetShow(true);
+      SetShow(false);
     }
   };
   return (
     <div className="wrapper">
-      <h1>Kliknij 5 razy</h1>
+      <h1>{count}</h1>
       <button className="someBtn" onClick={HandleClick}>
         click
       </button>
