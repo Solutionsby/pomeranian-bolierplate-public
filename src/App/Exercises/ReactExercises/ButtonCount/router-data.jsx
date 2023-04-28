afe6ead6c12f60f8680f148dd13bbe0a7cf54b79
Paweł Goc <pawel.goc@solutionsby.pl>
2023-04-28 17:09:53 +0200

    add new folder

---
+++ src/App/Exercises/ReactExercises/ButtonCount/router-data.jsx
@@ -0,0 +1,10 @@
+import { WelcomeView } from './WelcomView';
+
+export const welcomeVievRouterMetaData = {
+  path: 'WelcomeView',
+  date: '27-04-2023',
+  linkLabel: 'Welcome view',
+  blockNo: 1,
+  element: <WelcomeView />,
+  tags: ['components'],
+};
