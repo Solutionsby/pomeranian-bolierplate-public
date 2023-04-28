9a138dd00e5d82bb8a090424bab1020557ab0302
Paweł Goc <pawel.goc@solutionsby.pl>
2023-04-28 18:51:24 +0200

    another way

--- src/App/Exercises/ReactExercises/ButtonCount/ButtonCount.jsx
+++ src/App/Exercises/ReactExercises/ButtonCount/ButtonCount.jsx
@@ -2,20 +2,25 @@ import { useState } from 'react';
 import './styles.css';
 
 export const ButtonCount = () => {
-  let [count, setCount] = useState(0);
-  let x = 'wanish';
-  if (count >= 5) {
-    x = 'activeText';
-    count = 0;
-  }
+  let [count, setCount] = useState(1);
+  const [show, SetShow] = useState(false);
 
+  const HandleClick = () => {
+    if (count < 5) {
+      setCount(count + 1);
+      SetShow(false);
+    } else {
+      setCount(0);
+      SetShow(true);
+    }
+  };
   return (
     <div className="wrapper">
       <h1>Kliknij 5 razy</h1>
-      <button className="someBtn" onClick={() => setCount(count + 1)}>
-        Kliknełeś {count} razy
+      <button className="someBtn" onClick={HandleClick}>
+        click
       </button>
-      <div className={x}>Ukryta wiadomość </div>
+      {show && <p>Pokazuje się </p>}
     </div>
   );
 };
