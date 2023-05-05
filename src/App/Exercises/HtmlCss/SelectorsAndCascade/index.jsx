3fcef321ae5b4fc9cac61f5131eb7e0d3cb483e6
Paweł Goc <pawel.goc@solutionsby.pl>
2023-05-05 18:14:50 +0200

    Add new folder

---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/index.jsx
@@ -0,0 +1,5 @@
+import './styles.css';
+
+export function SelectorAndCascade() {
+  return <p>Ćwiczenia z HTML & CSS - 2</p>;
+}
---
+++ src/App/Exercises/HtmlCss/SelectorsAndCascade/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { SelectorAndCascade } from './index';
+
+export const blockRouterMetaData = {
+  path: 'Selector-and-Cascade',
+  date: '05-05-2023',
+  linkLabel: 'HTML & CSS - Kaskadowaosc specyficznosc i dziedziczenie ',
+  blockNo: 1,
+  element: <SelectorAndCascade />,
+  tags: ['flex', 'grid', 'video'],
+};
