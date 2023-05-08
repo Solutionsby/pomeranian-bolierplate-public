145a0e053b9829aae575c8074f643d7be9bf186d
Paweł Goc <pawel.goc@solutionsby.pl>
2023-05-08 19:01:52 +0200

    exercise in css

---
+++ src/App/Exercises/HtmlCss/StandardTaqs/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { StandardTags } from './index';
+
+export const blockRouterMetaData = {
+  path: 'Standard-tags',
+  date: '08-05-2023',
+  linkLabel: 'HTML & CSS - Tagi html ',
+  blockNo: 1,
+  element: <StandardTags />,
+  tags: ['flex', 'grid', 'video'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -2,9 +2,11 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as blockRouterMetaData5052023 } from './SelectorsAndCascade/router-data';
+import { blockRouterMetaData as blockRouterMetaData8052023 } from './StandardTaqs/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   blockRouterMetaData5052023,
+  blockRouterMetaData8052023,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
