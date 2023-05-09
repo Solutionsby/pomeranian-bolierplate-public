c2e2cb4f5c128f7bb07a6c6fbd3b6dd8af4843be
Paweł Goc <pawel.goc@solutionsby.pl>
2023-05-09 17:18:47 +0200

    Add structur and rout

---
+++ src/App/Exercises/HtmlCss/ColorsCss/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { ColorsCss } from './index';
+
+export const blockRouterMetaData = {
+  path: 'Colors-Css',
+  date: '09-05-2023',
+  linkLabel: 'HTML & CSS - Colors Css ',
+  blockNo: 1,
+  element: <ColorsCss />,
+  tags: ['flex', 'grid', 'video'],
+};
--- src/App/Exercises/HtmlCss/view-router-data.jsx
+++ src/App/Exercises/HtmlCss/view-router-data.jsx
@@ -3,10 +3,12 @@ import { getParsedRouterDataList } from '../../router-data/parseRouterData';
 import { blockRouterMetaData as blockRouterMetaData121220231 } from './Exercise-example-12-12-2023-1/router-data';
 import { blockRouterMetaData as blockRouterMetaData5052023 } from './SelectorsAndCascade/router-data';
 import { blockRouterMetaData as blockRouterMetaData8052023 } from './StandardTaqs/router-data';
+import { blockRouterMetaData as blockRouterMetaData9052023 } from './ColorsCss/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaData121220231,
   blockRouterMetaData5052023,
   blockRouterMetaData8052023,
+  blockRouterMetaData9052023,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
