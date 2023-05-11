ed000da65529fcbfc14ed989b6e3ad134607399e
Paweł Goc <pawel.goc@solutionsby.pl>
2023-05-11 18:12:26 +0200

    Make a table

---
+++ src/App/Exercises/HtmlCss/HtmlTebles/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Tables } from './tables';
+
+export const blockRouterMetaData = {
+  path: 'tables',
+  date: '11-05-2023',
+  linkLabel: 'HTML & CSS - Tabele',
+  blockNo: 1,
+  element: <Tables />,
+  tags: ['flex', 'grid', 'video'],
+};
