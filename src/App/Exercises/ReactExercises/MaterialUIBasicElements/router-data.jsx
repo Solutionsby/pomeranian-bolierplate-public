0abf9cf6b0a916eb0380f98aa1851360921c9e1f
Paweł Goc <pawel.goc@solutionsby.pl>
2023-05-16 18:05:10 +0200

    some Change

---
+++ src/App/Exercises/ReactExercises/MaterialUIBasicElements/router-data.jsx
@@ -0,0 +1,10 @@
+import { MaterialUIBasicElements } from './MaterialUIBasicElements';
+
+export const MaterialUIBasicElementsRouterMetaData = {
+  path: 'material-ui-basic-elements',
+  date: '16-05-2023',
+  linkLabel: 'EmptyComponentWithRouting',
+  blockNo: 16,
+  element: <MaterialUIBasicElements />,
+  tags: ['components'],
+};
