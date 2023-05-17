226fe2b39ec2e8a9654af23a39e14b334f4c2c72
Paweł Goc <pawel.goc@solutionsby.pl>
2023-05-17 19:42:03 +0200

    if exempel

--- src/App/Exercises/Js/Boolean/Boolean.jsx
+++ src/App/Exercises/Js/Boolean/Boolean.jsx
@@ -1,16 +1,32 @@
 import './styles.css';
 
 export const BooleanEx = () => {
-  let result = 10;
+  // let result = 10;
 
-  for (let i = 1; i <= 5; i++) {
-    result += i;
+  // for (let i = 1; i <= 5; i++) {
+  //   result += i;
+  // }
+  // console.log(result);
+  // const value1 = true;
+  // const value2 = true;
+  // const value3 = false;
+  // const condition = (value1 && value2) || value3;
+
+  let age = 19;
+  let hasDriverLicense = true;
+  let hasCar = true;
+
+  function canDrive(age, hasDriverLicense, hasCar) {
+    if (age < 18) {
+      return 'You are too young to drive.';
+    } else if (age >= 18 && !hasDriverLicense) {
+      return "You can't drive without a driver's license.";
+    } else if (age >= 18 && hasDriverLicense && !hasCar) {
+      return 'You can drive a rental car.';
+    } else {
+      return 'You can drive your car!';
+    }
   }
-  console.log(result);
-  const value1 = true;
-  const value2 = true;
-  const value3 = false;
-  const condition = (value1 && value2) || value3;
-
-  return <div>{condition && <h1>Tajny tekst </h1>}</div>;
+  console.log(canDrive(age, hasDriverLicense, hasCar));
+  return <div></div>;
 };
