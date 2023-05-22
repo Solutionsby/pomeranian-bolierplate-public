5264959839a047bd3306fcb9aeb68d46c819a97b
Paweł Goc <pawel.goc@solutionsby.pl>
2023-05-22 17:13:21 +0200

    folder structure

---
+++ src/App/Exercises/Js/JsFunction/jsFunction.jsx
@@ -0,0 +1,5 @@
+import './styles.css';
+
+export const JsFunction = () => {
+  return <div></div>;
+};
---
+++ src/App/Exercises/Js/JsFunction/router-data.jsx
@@ -0,0 +1,10 @@
+import { JsFunction } from './jsFunction';
+
+export const blockRouterMetaData = {
+  path: 'some-js-function-exercises',
+  date: '22-05-2023',
+  linkLabel: 'Js - function',
+  blockNo: 21,
+  element: <JsFunction />,
+  tags: ['function', 'js'],
+};
