42d8b5b631c177960dd58e9d224bd2c34d6181dc
Paweł Goc <pawel.goc@solutionsby.pl>
2023-06-12 20:28:27 +0200

    Promise

--- src/App/Exercises/ReactExercises/SideEffects/SideEffects.jsx
+++ src/App/Exercises/ReactExercises/SideEffects/SideEffects.jsx
@@ -1,5 +1,49 @@
+import { useEffect, useState } from 'react';
 import './styles.css';
 
 export const SideEffects = () => {
-  return <div></div>;
+  const [value, setValue] = useState([]);
+  // useEffect(() => {
+  //   console.log('UseEffect - na Value');
+  // }, [value]);
+  // const [age, setAge] = useState(0);
+  // useEffect(() => {
+  //   console.log('UseEffect - na Age');
+  // }, [age]);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     console.log('setTimeOut');
+  //   }, 2000);
+  //   console.log('without SetTmieout');
+  // }, []);
+  useEffect(() => {
+    const myPromise = new Promise((resolve, reject) => {
+      const number = Math.floor(Math.random() * 2);
+      console.log(number);
+      setTimeout(() => {
+        if (number === 1) {
+          resolve([{ name: 'Olga' }, { name: 'Lukas' }]);
+        } else {
+          reject([]);
+        }
+      }, 2000);
+    });
+
+    myPromise
+      .then((result) => {
+        setValue(result);
+      })
+      .catch((error) => {
+        setValue(error);
+      });
+  }, []);
+  return (
+    <div className="side-effects">
+      <ul>
+        {value.map((item) => (
+          <li kay={item.name}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
