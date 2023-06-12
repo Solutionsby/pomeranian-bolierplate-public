e2434fd8c736e7664f7f858771a130362d8fbe20
Paweł Goc <pawel.goc@solutionsby.pl>
2023-06-12 17:11:31 +0200

    SideEffect routs

---
+++ src/App/Exercises/ReactExercises/SideEffects/router-data.jsx
@@ -0,0 +1,10 @@
+import { SideEffects } from './SideEffects';
+
+export const SideEffectsData = {
+  path: 'side-effects',
+  date: '12-06-2023',
+  linkLabel: 'Side-effects',
+  blockNo: 1,
+  element: <SideEffects />,
+  tags: ['components'],
+};
--- src/App/Exercises/ReactExercises/view-router-data.jsx
+++ src/App/Exercises/ReactExercises/view-router-data.jsx
@@ -9,6 +9,7 @@ import { TwoInputVievRouterMetaData } from './TwoInput/router-data';
 import { welcomeVievRouterMetaData } from './WelcomView/router-data';
 import { GalleryVievRouterMetaData } from './Gallery/router-data';
 import { MaterialUIBasicElementsRouterMetaData } from './MaterialUIBasicElements/router-data';
+import { SideEffectsData } from './SideEffects/router-data';
 
 export const blockRouterMetaData = [
   SubRouteExampleMetaData,
@@ -20,5 +21,6 @@ export const blockRouterMetaData = [
   RandomNumberVievRouterMetaData,
   GalleryVievRouterMetaData,
   ...MaterialUIBasicElementsRouterMetaData,
+  SideEffectsData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
