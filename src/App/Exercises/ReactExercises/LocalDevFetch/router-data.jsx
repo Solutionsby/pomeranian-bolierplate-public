fffb7e3a21572d659c907e87c7a2fe95e0d16c67
Paweł Goc <pawel.goc@solutionsby.pl>
2023-06-14 18:57:31 +0200

    folders

---
+++ src/App/Exercises/ReactExercises/LocalDevFetch/router-data.jsx
@@ -0,0 +1,10 @@
+import { LocalDevAndFetch } from './LocalDevFetch';
+
+export const LocalDevFetchMetaData = {
+  path: 'local-dev-and-fetchem',
+  date: '12-12-2023',
+  linkLabel: 'Local-dev-fetch',
+  blockNo: 1,
+  element: <LocalDevAndFetch />,
+  tags: ['endpoint', 'fetch', 'rest', 'api', 'javascript'],
+};
